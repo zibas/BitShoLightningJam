e10f8433b108565205b7bd6bb54ce069a6d01367
DESKTOP-ASUS\rohit <rohitcrasta@gmail.com>
2020-08-20 21:07:27 -0400

    Initial commit of project (2018.4.2f1)

---
+++ Unity/BitShoGame/Assets/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject WordButtonPrefab;
+
+    private List<GameObject> _wordStuffs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(WordButtonPrefab, transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
